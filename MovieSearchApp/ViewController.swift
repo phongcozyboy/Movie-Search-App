96c31525fe21013538617e7ec33ca11cd522012a
kid43 <hphongug0210@gmail.com>
2021-06-14 16:05:49 +0700

    Initial Commit

---
+++ MovieSearchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieSearchApp
+//
+//  Created by Phong Le on 14/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
